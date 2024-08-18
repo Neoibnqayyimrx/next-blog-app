866f3fee7555a21023c7a3e2766e5ffc40935db1
Neoibnqayyimrx <neoibnqayyim@hotmail.com>
2024-08-18 16:24:43 +0100

    completed blog item page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
+'use client'
+
+ 
+import BlogList from "@/components/BlogList";
 import Header from "@/components/Header";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <>
     <Header />
+   <BlogList />
     </>
       );
 }
